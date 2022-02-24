667640e2b35159835083863083859d85789ae5fb
Дмитрий Кодолов <dim.kodolov@yandex.ru>
2022-02-24 13:53:37 +0400

    integrated the token into the request

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL_DEV,
+});
+
+instance.interceptors.request.use((config) => {
+  config.headers!.Authorization = `${Cookies.get('accessToken')}`;
+  return config;
 });
 
 export default instance;
